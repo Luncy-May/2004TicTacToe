2794747521c62b706feb9f2946550e0958b70e1a
LUNCY579 <clclchengli@gmail.com>
2025-11-09 12:42:22 -0600

    set a place holder value X

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 function Square() {
   const [value, setValue] = useState(null);
   function handleClick() {
+    setValue('X');
     console.log('clicked!');
   }
   return <button className="square" onClick={handleClick}>{value}</button>;
